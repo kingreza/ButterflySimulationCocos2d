85d59e7a320b5da0f8912e91c14eb1bacde67762
Reza Shirazian <reza.shirazian@frontrowsoft.com>
2014-02-03 17:34:01 -0700

    Initial Commit

---
+++ ButterflyAnimation/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  ButterflyAnimation
+//
+//  Created by Reza Shirazian on 2/3/2014.
+//  Copyright Reza Shirazian 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ ButterflyAnimation/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ButterflyAnimation
+//
+//  Created by Reza Shirazian on 2/3/2014.
+//  Copyright Reza Shirazian 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
